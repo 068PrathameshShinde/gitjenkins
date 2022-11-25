1fc56898046aad60a230efb79b8df58ae2a61747
Prathamesh Shinde <prathamesh.shinde@morningstar.com>
2022-11-25 10:58:44 +0530

    code commit

---
+++ com/demo/Demofile.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import java.util.Iterator;
+
+public class Demofile {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <5; i++) {
+			
+			System.out.println("hello "+i);
+		}
+	}
+}
